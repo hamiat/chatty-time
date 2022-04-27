416fdde1c888ced9d360948ead88956feff737f8
Hamiat <hamiat.n@hotmail.com>
2022-04-27 12:35:42 +0200

    wip: uselogout hook created

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+    const [error, setError] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext;
+
+    const logout = async () => {
+        setError(null);
+        setIsPending(false);
+
+        //sign user out
+        try {
+            await projectAuth.signOut();
+
+            //dispatch logout action
+            dispatch({ type: "LOGOUT" });
+
+            setIsPending(null);
+            setError("Logged out!");
+        } catch (err) {
+            console.log(err.message);
+            setError(null);
+            setIsPending(false);
+        }
+    };
+    return { logout, error, isPending };
+};
