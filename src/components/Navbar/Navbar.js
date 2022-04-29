4c5716d9a225b5ca167b991a0176485069861371
Hamiat <hamiat.n@hotmail.com>
2022-04-29 14:37:39 +0200

    wip:using user argument to conditionally render nav bar actions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,42 @@
 import styles from "./navbar.module.scss";
 import { useAuthentication } from "../../hooks/useAuthentication";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Navbar() {
     const { logout } = useAuthentication();
+    const { user } = useAuthContext();
+
     return (
         <>
             <h1>Chatty Time</h1>
             <nav>
                 <ul>
-                    <li className={styles.links}>
-                        <a href="/">Login</a>
-                    </li>
-                    <li className={styles.links}>
-                        <a href="/">Signup</a>
-                    </li>
-                    <li>
-                        <button className={styles.logoutBtn} onClick={logout}>
-                            Logout
-                        </button>
-                    </li>
+                    {!user && (
+                        <>
+                            <li className={styles.links}>
+                                <a href="/">Login</a>
+                            </li>
+                            <li className={styles.links}>
+                                <a href="/">Signup</a>
+                            </li>
+                        </>
+                    )}
+
+                    {user && (
+                        <>
+                            <li className={styles.helloUser}>
+                                Hello {user.displayName}
+                            </li>
+                            <li>
+                                <button
+                                    className={styles.logoutBtn}
+                                    onClick={logout}
+                                >
+                                    Logout
+                                </button>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </>
